cc7c8c31d880c13940634ffa3559a00db366bb51
Spunksai@09 <b.jayaditya@gmail.com>
2021-05-28 09:59:39 +0530

    Data Types

---
+++ src/datatypes/empdetais.java
@@ -0,0 +1,33 @@
+package datatypes;
+
+import java.util.Scanner;
+
+public class empdetais {
+
+	public static void main(String[] args) {
+		Scanner m = new Scanner(System.in);
+		byte empId = 90;
+        String empName = "aditya";
+        String empEmail ="b.jayadtya@gmail.com";
+        long empPhoneno = 8008316863l;
+        float empSalary = 4000.45f;
+        char empGender='M';
+        String empCity="Chennai";
+        
+        System.out.println("Id is "+empId);
+        System.out.println("Name is "+empName);
+        System.out.println("Email ID is "+empEmail);
+        System.out.println("Contact No is "+empPhoneno);
+        System.out.println("Salary is "+empSalary);
+        System.out.println("Gender "+empGender);
+        System.out.println("Place is "+empCity);
+        
+        for(int i=0;i<10;i++) {
+        	System.out.println(i);
+        }
+       
+	}
+	
+	
+
+}
